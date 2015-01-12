467d9c6af3a740b182e61360dd11f79527e9beab
clonn <clonncd@gmail.com>
2015-01-12 18:53:52 +0800

    init git

---
+++ app/index.js
@@ -0,0 +1,99 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var fs = require('fs-extra');
+
+module.exports = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+    this.fsExtra = require('fs-extra');
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the stunning' + chalk.red('SailsNanzi') + ' generator!\n' +
+      'It is base on Sails.js, collection coffeescript, jade, compass'
+    ));
+
+    this.prompt([{
+      type    : 'input',
+      name    : 'name',
+      message : 'What is your project name (default is current folder name)?',
+      default : this.appname // Default to current folder name
+    },
+    {
+      type    : 'confirm',
+      name    : 'ready',
+      message : 'Are you ready to create project?',
+      default : true
+    }], function (answers) {
+      this.name = answers.name;
+      this.ready = answers.ready;
+      done();
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+
+      this.fsExtra.copy(
+        this.templatePath('./templates/'),
+        this.destinationPath('.'),
+        function(err) {
+          if (err) return console.error(err)
+
+          this.fs.copyTpl(
+            this.templatePath('package.json'),
+            this.destinationPath('package.json'),
+            { name: this.name }
+          );
+
+          this.fs.copyTpl(
+            this.templatePath('bower.json'),
+            this.destinationPath('bower.json'),
+            { name: this.name }
+          );
+
+          this.fs.copyTpl(
+            this.templatePath('_README.md'),
+            this.destinationPath('README.md'),
+            { name: this.name }
+          );
+
+          console.log("success!")
+      });
+
+
+      
+      // this.fs.copy(
+      //   this.templatePath('_package.json'),
+      //   this.destinationPath('package.json')
+      // );
+      // this.fs.copy(
+      //   this.templatePath('_bower.json'),
+      //   this.destinationPath('bower.json')
+      // );
+    },
+
+    projectfiles: function () {
+      this.fs.copy(
+        this.templatePath('editorconfig'),
+        this.destinationPath('.editorconfig')
+      );
+      this.fs.copy(
+        this.templatePath('jshintrc'),
+        this.destinationPath('.jshintrc')
+      );
+    }
+  },
+
+  install: function () {
+    this.installDependencies({
+      skipInstall: this.options['skip-install']
+    });
+  }
+});
---
+++ app/templates/config.rb
@@ -0,0 +1,5 @@
+require 'bootstrap-sass'
+require 'compass'
+images_dir = "assets/styles/images/"
+sass_dir = "assets/styles/"
+css_dir = ".tmp/public/styles/"
